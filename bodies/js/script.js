4d2641db1bf18f6c5cea89e2a0c91a3369ee9e78
GoddessC <goddesselixir@gmail.com>
2015-03-08 18:30:36 -0400

    Initial slider style and function

--- bodies/js/script.js
+++ bodies/js/script.js
@@ -10,6 +10,19 @@ $('.burger').click(function(){
 });
 
 
+// Initiates Slider
+$('.flexslider').flexslider({
+      animation: "fade",
+      pauseOnAction: true,
+      slideDirection: "Vertical",
+      slideshowSpeed: 5000,
+      prevText: "&#x276e;",
+      nextText: "&#x276f;",
+      directionNav: false,
+      controlNav: false,
+      animationDuration: 10
+});
+
 
 
 }
